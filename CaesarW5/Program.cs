0018e132d8545cd23af5cb41de51f8293d2778c4
Maxim Pozdniakov <mpozdniakov@kse.org.ua>
2022-10-27 21:59:55 +0300

    FileReader and Writer usage implementation in the program

--- CaesarW5/Program.cs
+++ CaesarW5/Program.cs
@@ -1,9 +1,13 @@
 ﻿using Caesar;
+using CaesarW5;
+
+FileWriter writer = new FileWriter();
+FileReader reader = new FileReader();
 
 while (true)
 {
-    Console.WriteLine("Write text:");
-    char[] text = Console.ReadLine().ToCharArray();
+    Console.WriteLine("Write path to read file from:");
+    char[] text = reader.Read(Console.ReadLine());
     Console.WriteLine("Write key:");
     var key = Console.ReadLine();
     Console.WriteLine("Write 1 if encrypt and 2 if decrypt");
@@ -18,4 +22,15 @@ while (true)
         result = crypter.Decrypt(text, int.Parse(key));
     }
     crypter.Print(result);
+    Console.WriteLine("Do you wanna save result?");
+    if (Console.ReadLine() == "yes")
+    {
+        Console.WriteLine("Enter path to save:");
+        writer.Write(result, Console.ReadLine());
+    }
+    Console.WriteLine("Do you wanna finish?");
+    if (Console.ReadLine() == "yes")
+    {
+        break;
+    }
 }
