61cc78e073c0ac2f7fa64d30f6a4de9a28077a04
Maxim Pozdniakov <mpozdniakov@kse.org.ua>
2022-10-27 21:29:09 +0300

    Base commit. Default implementation from last practice using library

---
+++ CaesarW5/Program.cs
@@ -0,0 +1,21 @@
+﻿using Caesar;
+
+while (true)
+{
+    Console.WriteLine("Write text:");
+        char[] text = Console.ReadLine().ToCharArray();
+        Console.WriteLine("Write key:");
+        var key = Console.ReadLine();
+        Console.WriteLine("Write 1 if encrypt and 2 if decrypt");
+        var result = new char[] { };
+        var crypter = new CaesarCrypt();
+        if (Console.ReadLine() == "1")
+        {
+            result = crypter.Encrypt(text, int.Parse(key));
+        }
+        else
+        {
+            result = crypter.Decrypt(text, int.Parse(key));
+        }
+        crypter.Print(result);
+}
